88130db067fea4666d2ca3dfb421900b02fd3346
xiaoxubo <xiaoxubo@shbaoyuantech.com>
2018-01-16 16:20:48 +0800

    add code

--- pages/list/list.js
+++ pages/list/list.js
@@ -6,18 +6,18 @@ var util = require('../../utils/util.js');
 Page({
   data:{
     // text:"这是一个页面"
-    list: [{ firstImg: "../image/icon_foot.png", title: "上海市", source: "东方明珠"},
-      { firstImg: "../image/icon_foot.png", title: "上海市", source: "东方明珠" },
-      { firstImg: "../image/icon_foot.png", title: "上海市", source: "东方明珠" },
-      { firstImg: "../image/icon_foot.png", title: "上海市", source: "东方明珠" },
-      { firstImg: "../image/icon_foot.png", title: "上海市", source: "东方明珠" },
-      { firstImg: "../image/icon_foot.png", title: "上海市", source: "东方明珠" },
-      { firstImg: "../image/icon_foot.png", title: "上海市", source: "东方明珠" },
-      { firstImg: "../image/icon_foot.png", title: "上海市", source: "东方明珠" },
-      { firstImg: "../image/icon_foot.png", title: "上海市", source: "东方明珠" },
-      { firstImg: "../image/icon_foot.png", title: "上海市", source: "东方明珠" },
-      { firstImg: "../image/icon_foot.png", title: "上海市", source: "东方明珠" },
-      { firstImg: "../image/icon_foot.png", title: "上海市", source: "东方明珠" },],
+    list: [{ firstImg: "../image/scan.png", title: "上海市", source: "东方明珠"},
+      { firstImg: "../image/scan.png", title: "上海市", source: "东方明珠" },
+      { firstImg: "../image/scan.png", title: "上海市", source: "东方明珠" },
+      { firstImg: "../image/scan.png", title: "上海市", source: "东方明珠" },
+      { firstImg: "../image/scan.png", title: "上海市", source: "东方明珠" },
+      { firstImg: "../image/scan.png", title: "上海市", source: "东方明珠" },
+      { firstImg: "../image/scan.png", title: "上海市", source: "东方明珠" },
+      { firstImg: "../image/scan.png", title: "上海市", source: "东方明珠" },
+      { firstImg: "../image/scan.png", title: "上海市", source: "东方明珠" },
+      { firstImg: "../image/scan.png", title: "上海市", source: "东方明珠" },
+      { firstImg: "../image/scan.png", title: "上海市", source: "东方明珠" },
+      { firstImg: "../image/scan.png", title: "上海市", source: "东方明珠" },],
      dd:'',
      hidden:false,
      page: 1,
@@ -26,41 +26,27 @@ Page({
      hasRefesh:false
   },
   onLoad:function(options){
-   console.log(md5.hex_md5('123456'));
-    // console.log(d);
-    // var that = this;
-    // var url = 'http://v.juhe.cn/weixin/query?key=f16af393a63364b729fd81ed9fdd4b7d&pno=1&ps=10';
-    // network_util._get(url,
-    // function(res){
-    // that.setData({
-    //    list:res.data.result.list,
-    //    hidden: true,
-    // });
-    // },function(res){
-    //  console.log(res);
-    
-//  });
+   console.log(options);
 
+  let map = new Map();
+  // map.set('username','13261570828');
+  // map.set('password','123456');
+  // map.set('orgid','0010000');0010000022464
+  map.set('customerid','0010000022464');
+  let d = json_util.mapToJson(util.tokenAndKo(map));
+  console.log(d);
+  var url1 = 'http://127.0.0.1:8088/getUserBindingDevice';
+  var that = this;
+  network_util._post(url1,d,
+  function(res){
+      console.log(res);
+      },function(res){
+        that.setData({
+          hidden: true,
+        });
+      console.log(res);
 
-let map = new Map();
-// map.set('username','13261570828');
-// map.set('password','123456');
-// map.set('orgid','0010000');0010000022464
-map.set('customerid','0010000022464');
-let d = json_util.mapToJson(util.tokenAndKo(map));
-console.log(d);
- var url1 = 'http://127.0.0.1:8088/getUserBindingDevice';
- var that = this;
-network_util._post(url1,d,
-function(res){
-     console.log(res);
-    },function(res){
-      that.setData({
-        hidden: true,
-      });
-     console.log(res);
-
- });
+  });
   },
   onReady:function(){
     // 页面渲染完成
